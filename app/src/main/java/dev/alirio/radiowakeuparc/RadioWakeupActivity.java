a70d649697cee50992e29eebd11bc29234494257
aliriorivera <ariveracu@unal.edu.co>
2018-09-03 00:39:58 -0400

    adding UI widgets and responsive ability for the app

--- app/src/main/java/dev/alirio/radiowakeuparc/RadioWakeupActivity.java
+++ app/src/main/java/dev/alirio/radiowakeuparc/RadioWakeupActivity.java
@@ -2,6 +2,7 @@ package dev.alirio.radiowakeuparc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RadioWakeupActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class RadioWakeupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio_wakeup);
+    }
+
+
+    public void setRadioAlarmStart(View view) {
+
+    }
+
+    public void setRadioAlarmStop(View view){
+
     }
 }
