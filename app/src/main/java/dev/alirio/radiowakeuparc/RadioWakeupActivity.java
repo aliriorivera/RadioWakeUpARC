5f9cac683ed1a4309f04a037f28f8660d9f89a1f
aliriorivera <ariveracu@unal.edu.co>
2018-09-03 00:08:07 -0400

    adding base Android files and settings

---
+++ app/src/main/java/dev/alirio/radiowakeuparc/RadioWakeupActivity.java
@@ -0,0 +1,13 @@
+package dev.alirio.radiowakeuparc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadioWakeupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_wakeup);
+    }
+}
